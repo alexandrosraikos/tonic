fecbb403a22167d7c536bf0ccc09220342674d74
Alexandros Raikos <alexandros@dood.gr>
2023-02-24 08:46:43 +0200

    view controller cleanup

--- src/View/Controller.php
+++ src/View/Controller.php
@@ -11,20 +11,7 @@ class Controller extends Blade
     public function __construct(string $viewPaths, string $cachePath, string $componentsPath)
     {
         parent::__construct($viewPaths, $cachePath);
-        $this->teach();
         $this->attach($componentsPath);
-    }
-
-    protected function teach(): void
-    {
-        $this->directive(
-            'translate',
-            fn ($text) => "<?php echo \ICOSE\InspiralReader\Feature\Multilanguage::translate($text); ?>"
-        );
-        $this->directive(
-            'verbose',
-            fn ($bool) => "<?php echo boolval($bool) ? 'Yes' : 'No'; ?>"
-        );
     }
 
     protected function attach(string $path): void
